e969c48b30176e3db672f216ca0108fccd447770
ugnius <ugnius.vilkoncius@gmail.com>
2024-06-01 21:46:41 +0300

    Movement to docker

--- DocsManager/Program.cs
+++ DocsManager/Program.cs
@@ -4,28 +4,36 @@ using DocsManager.Utils.DocsGenerator;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 Thread.CurrentThread.CurrentCulture = new CultureInfo("lt-LT");
 var builder = WebApplication.CreateBuilder(args);
-
+var configuration = builder.Configuration;
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(x =>
     {
         x.IncludeErrorDetails = true;
-        x.Authority = "http://localhost:8080/realms/DocsManagement";
-        x.MetadataAddress =
-            "http://localhost:8080/realms/DocsManagement/.well-known/openid-configuration"; 
+        x.Authority = configuration["auth:authorityServer"];
+        x.MetadataAddress = configuration["auth:authorityServer"] + "/.well-known/openid-configuration";
+        x.Audience = "api";
         x.RequireHttpsMetadata = false;
         x.TokenValidationParameters = new TokenValidationParameters
         {
             RoleClaimType = "groups",
-            //NameClaimType = $"{configuration["Keycloak:name_claim"]}",
-            //ValidAudience = "DocsManagementReact,account", 
-            // https://stackoverflow.com/questions/60306175/bearer-error-invalid-token-error-description-the-issuer-is-invalid
-            ValidateIssuer = false,
-            ValidateAudience = false
+            ValidateIssuerSigningKey = false,
+            SignatureValidator = delegate (string token, TokenValidationParameters parameters)
+            {
+                var jwt = new JsonWebToken(token);
+
+                return jwt;
+            },
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = configuration["auth:authorityServer"],
+            ValidAudience = "account"
         };
     });
 
@@ -37,19 +45,18 @@ builder.Services.AddAuthorization(o =>
 });
 
 builder.Services.AddCors(options => options.AddPolicy(name: "DocManagerUi",
-    policy => { policy.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowAnyHeader(); }));
+    policy => { policy.WithOrigins(configuration["frontend:server"]).AllowAnyMethod().AllowAnyHeader(); }));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<DocsManagementContext>(optionsBuilder =>
 {
-    optionsBuilder.UseNpgsql(
-        @"Host=192.168.1.115;Database=docsmanagement;Username=postgres;Password=homeassistant");
+    optionsBuilder.UseNpgsql(configuration["dbstring"]);
 });
 builder.Services.AddScoped<IHtmlGenerator, Template>();
 builder.Services.AddScoped<IPdfGenerator, HtmlToPdf>();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Projects-admin Swagger", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DocsManager Swagger", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
@@ -74,6 +81,17 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<DocsManagementContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
