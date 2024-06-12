06d5bbf34561b4567cb00c30b276cf5d32c55246
ugnius <ugnius.vilkoncius@gmail.com>
2024-06-12 21:56:18 +0300

    Return total sum from invoice as well

--- DocsManager/Models/Dto/InvoiceListDto.cs
+++ DocsManager/Models/Dto/InvoiceListDto.cs
@@ -1,16 +1,3 @@
 namespace DocsManager.Models.Dto;
 
-public record InvoiceListDto
-{
-    public InvoiceListDto(int invoiceId, DateTime invoiceDate, string clientName)
-    {
-        InvoiceId = invoiceId;
-        InvoiceDate = invoiceDate;
-        ClientName = clientName;
-    }
-
-
-    public int InvoiceId { get; }
-    public DateTime InvoiceDate { get; }
-    public string ClientName { get; }
-}
+public record InvoiceListDto(int InvoiceId, DateTime InvoiceDate, string ClientName, decimal TotalSum);
