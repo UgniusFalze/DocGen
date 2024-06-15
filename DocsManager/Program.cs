78c29b841163b6805c9060eaf797001882461b3a
ugnius <ugnius.vilkoncius@gmail.com>
2024-06-15 20:45:50 +0300

    Program cs cleanup

--- DocsManager/Program.cs
+++ DocsManager/Program.cs
@@ -1,100 +1,17 @@
-using System.Globalization;
 using DocsManager.Models;
-using DocsManager.Services.Client;
-using DocsManager.Services.IntegerToWordsConverter;
-using DocsManager.Services.Invoice;
-using DocsManager.Utils.DocsGenerator;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
+using DocsManager.Utils;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.JsonWebTokens;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using QuestPDF.Infrastructure;
-using Serilog;
-QuestPDF.Settings.License = LicenseType.Community;
-Thread.CurrentThread.CurrentCulture = new CultureInfo("lt-LT");
-var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
 
-builder.Host.UseSerilog(((context, loggerConfiguration) => 
-        {
-            loggerConfiguration.ReadFrom.Configuration(configuration);
-        }
-        ));
+var docManagerAppBuilder = new DocManagerAppBuilder(args)
+    .ConfigureQuestPdf()
+    .ConfigureLogging()
+    .ConfigureAuth()
+    .ConfigureEndpoints()
+    .ConfigureDbContext()
+    .ConfigureInternalServices()
+    .ConfigureControllers();
 
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(x =>
-    {
-        x.IncludeErrorDetails = true;
-        x.Authority = configuration["auth:authorityServer"];
-        x.MetadataAddress = configuration["auth:authorityServer"] + "/.well-known/openid-configuration";
-        x.Audience = "api";
-        x.RequireHttpsMetadata = false;
-        x.TokenValidationParameters = new TokenValidationParameters
-        {
-            RoleClaimType = "groups",
-            ValidateIssuerSigningKey = false,
-            SignatureValidator = delegate(string token, TokenValidationParameters parameters)
-            {
-                var jwt = new JsonWebToken(token);
-
-                return jwt;
-            },
-            ValidateIssuer = true,
-            ValidateLifetime = true,
-            ValidIssuer = configuration["auth:authorityServer"],
-            ValidateAudience = false
-        };
-    });
-
-builder.Services.AddAuthorization(o =>
-{
-    o.DefaultPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-});
-
-builder.Services.AddCors(options => options.AddPolicy("DocManagerUi",
-    policy => { policy.WithOrigins(configuration["frontend:server"]).AllowAnyMethod().AllowAnyHeader(); }));
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<DocsManagementContext>(optionsBuilder =>
-{
-    optionsBuilder.UseNpgsql(configuration["dbstring"]);
-});
-builder.Services.AddScoped<IPdfGenerator, PdfGenerator>();
-builder.Services.AddScoped<IntegerToWordsConverter, LithuanianIntegerToWords>();
-builder.Services.AddScoped<IClientService, ClientService>();
-builder.Services.AddScoped<IInvoiceService, InvoiceService>();
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DocsManager Swagger", Version = "v1" });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please provide JWT with bearer (Bearer {jwt token})",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new List<string>()
-        }
-    });
-});
-var app = builder.Build();
+var app = docManagerAppBuilder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
