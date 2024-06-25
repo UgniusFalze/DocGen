70dbec3ff21e4320734b18b086e24b67fcab130e
ugnius <ugnius.vilkoncius@gmail.com>
2024-06-25 18:05:02 +0300

    sf

--- DocsManager/Controllers/PdfDocumentController.cs
+++ DocsManager/Controllers/PdfDocumentController.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using DocsManager.Models;
 using DocsManager.Models.Dto;
 using DocsManager.Services.IntegerToWordsConverter;
+using DocsManager.Services.Invoice;
 using DocsManager.Utils.DocsGenerator;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +17,7 @@ namespace DocsManager.Controllers;
 public class PdfDocumentController(
     IPdfGenerator pdfGenerator,
     DocsManagementContext _docsManagementContext,
+    IInvoiceService invoiceService,
     IntegerToWordsConverter itwc) : ControllerBase
 {
     private Guid? GetUserGuid()
@@ -30,26 +32,9 @@ public class PdfDocumentController(
     public async Task<IActionResult> DownloadFile(int invoiceId)
     {
         var userId = GetUserGuid();
-        var invoice = await _docsManagementContext.Invoices
-            .Where(x => x.InvoiceUserId == userId)
-            .Where(x => x.SeriesNumber == invoiceId)
-            .Select(x => new InvoiceDto
-            {
-                SeriesNumber = x.SeriesNumber,
-                Address = x.InvoiceUser.Address,
-                BankName = x.InvoiceUser.BankName,
-                BankNumber = x.InvoiceUser.BankNumber,
-                BuyerAddress = x.InvoiceClient.BuyerAddress,
-                BuyerCode = x.InvoiceClient.BuyerCode,
-                VatCode = x.InvoiceClient.VatCode,
-                BuyerName = x.InvoiceClient.BuyerName,
-                Date = x.InvoiceDate.ToString("yyyy MM dd"),
-                FreelanceWorkId = x.InvoiceUser.FreelanceWorkId,
-                Name = x.InvoiceUser.FirstName + " " + x.InvoiceUser.LastName,
-                Products = x.Items.Select(x => new ItemDto(x)).ToList()
-            })
-            .FirstOrDefaultAsync();
-        if (invoice == null) return NotFound();
+        if (userId == null) return NotFound("User not found");
+        var invoice = await invoiceService.GetInvoice(invoiceId, userId.Value);
+        if (invoice == null) return NotFound("Invoice not found");
         var totalCost = invoice.Products.Sum(product => product.TotalPrice);
         invoice.TotalMoney = totalCost.ToString("N2", CultureInfo.CreateSpecificCulture("lt-LT"));
         invoice.SumInWords = itwc.ConvertSumToWords(totalCost);
